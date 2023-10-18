5db62f8102a7584d8d7cbb0e838d614342d44ef9
Erdapin <salasespinozaew@gmail.com>
2023-10-18 14:48:40 -0600

    Collision Detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,10 +11,36 @@ public class Player : MonoBehaviour
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x,0f,inputVector.y);
-        isWalking = moveDir != Vector3.zero;
 
+        isWalking = moveDir != Vector3.zero;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-        transform.position += moveDir * Time.deltaTime * moveSpeed;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadious = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position,transform.position + Vector3.up * playerHeight, playerRadious,moveDir, moveDistance);
+        if (!canMove) {
+            //only x
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadious, moveDirX, moveDistance);
+            if (canMove)
+            {
+                //Can only on x
+                moveDir = moveDirX;
+            }
+            else {
+                // attempt only z
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadious, moveDirZ, moveDistance);
+                if (canMove) {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+
+        if (canMove) { 
+        transform.position += moveDir * moveDistance;
+        }
     }
 
     public bool IsWalking() { 
