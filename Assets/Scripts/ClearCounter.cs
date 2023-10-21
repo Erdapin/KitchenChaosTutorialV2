2d5d0b54d769b5782427533767815ae808dedc4f
Erdapin <salasespinozaew@gmail.com>
2023-10-20 22:59:47 -0600

    Change SO parent fix

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,14 +1,54 @@
 using UnityEngine;
+using UnityEngine.TerrainUtils;
 
 public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private KiitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform topSection;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
+
+    private KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T)) {
+            if (kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(secondClearCounter); 
+            }
+        }
+    }
+
     public void Interact()
     {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, topSection);
-        kitchenObjectTransform.localPosition = Vector3.zero;
-
-        kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO();
+        if(kitchenObject == null) { 
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, topSection);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
     }
+
+    public Transform GetKitchenObjectFollowTransform() {
+        return topSection;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject() {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
+
 }
